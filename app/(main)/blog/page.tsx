91e98dd325a22456db6a49d8fe699c109fb2dd3d
smuzj <918939601@qq.com>
2026-03-17 22:56:46 +0800

    继续优化

--- app/(main)/blog/page.tsx
+++ app/(main)/blog/page.tsx
@@ -1,7 +1,7 @@
 import BlogListPage from '@/modules/main/page/blog-list-page'
 import { getCachedBlogListData } from '@/lib/article-page-data'
 
-export const revalidate = 300
+export const dynamic = 'force-dynamic'
 
 export default async function Page() {
   const blogs = await getCachedBlogListData()
--- app/(main)/note/page.tsx
+++ app/(main)/note/page.tsx
@@ -1,7 +1,7 @@
 import NoteListPage from '@/modules/main/page/note-list-page'
 import { getCachedNoteListData } from '@/lib/article-page-data'
 
-export const revalidate = 300
+export const dynamic = 'force-dynamic'
 
 export default async function Page() {
   const notes = await getCachedNoteListData()
